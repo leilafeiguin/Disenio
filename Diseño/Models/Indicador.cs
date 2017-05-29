b7b95d5857ef64370b54d141621a6686752ac7a4
Leila Feiguin <lfeiguin@baufest.com>
2017-05-29 10:16:10 -0300

    Inicio pestaña indicadores

---
+++ Diseño/Models/Indicador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Diseño.Models
+{
+    public class Indicador
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Formula { get; set; }
+    }
+
+    public class IndicadorDBContext : DbContext
+    {
+        public DbSet<Indicador> Indicadores { get; set; }
+    }
+}
