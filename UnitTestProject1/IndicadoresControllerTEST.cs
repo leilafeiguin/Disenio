5bfda01d4f7ca0641726dadc91b0d46370c5e480
erratchumatias <erratchumatias@gmail.com>
2017-09-20 10:50:42 -0300

    Comienzo de Test
    Se comienza el desarrollo de TEST

---
+++ UnitTestProject1/IndicadoresControllerTEST.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class IndicadoresControllerTEST
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
+
+
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("daaf2314-f55d-4d2d-8693-47f0a8ecec92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
